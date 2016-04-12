f56b86b4291224aa1b5ad95b3ddecfde6b2380bd
Valeriy Kostikov <serbenv@gmail.com>
2016-04-12 23:45:09 +0300

    add react

--- routes.go
+++ routes.go
@@ -5,5 +5,13 @@ import (
 )
 
 func root(c *gin.Context) {
+	c.HTML(200, "index", gin.H{
+		"title": "Movie RSS",
+	})
+}
 
+func react(c *gin.Context) {
+	c.HTML(200, "react", gin.H{
+		"title": "React Tutorial",
+	})
 }
