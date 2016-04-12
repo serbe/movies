1b9070d3a4c85db56ec0af5759766040d8d6f65e
Valeriy Kostikov <serbenv@gmail.com>
2016-04-12 23:45:50 +0300

    add render templates

--- net.go
+++ net.go
@@ -1,20 +1,33 @@
 package main
 
 import (
+	"github.com/gin-gonic/contrib/renders/multitemplate"
 	"github.com/gin-gonic/gin"
 )
 
-func initServer() *gin.Engine {
+func initRender() multitemplate.Render {
+	r := multitemplate.New()
+
+	r.AddFromFiles("index", "templates/base.html", "templates/index.html")
+	r.AddFromFiles("react", "templates/react.html")
+
+	return r
+}
+
+func (app *application) initServer() {
 	r := gin.New()
+	r.HTMLRender = initRender()
+
 	r.Use(gin.Logger())
 	r.Use(gin.Recovery())
 
 	r.GET("/", root)
+	r.GET("/react", react)
 
 	r.Static("/public", "./public")
 	r.StaticFile("/favicon.ico", "./public/favicon.ico")
 
-	r.Run(":8080")
+	r.Run(":" + app.config.Web.Port)
 
-	return r
+	app.server = r
 }
