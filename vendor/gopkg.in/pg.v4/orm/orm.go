cdaffa0dc08838d3453a8825653f620b81a7a320
Valeriy Kostikov <serbenv@gmail.com>
2016-09-17 18:15:43 +0300

    update vendor

--- vendor/gopkg.in/pg.v4/orm/orm.go
+++ vendor/gopkg.in/pg.v4/orm/orm.go
@@ -19,8 +19,6 @@ type Collection interface {
 
 	// AddModel adds ColumnScanner to the Collection.
 	AddModel(ColumnScanner) error
-
-	AfterSelect(DB) error
 }
 
 type QueryAppender interface {
@@ -33,9 +31,16 @@ type QueryFormatter interface {
 
 // DB is a common interface for pg.DB and pg.Tx types.
 type DB interface {
-	Exec(q interface{}, params ...interface{}) (*types.Result, error)
-	ExecOne(q interface{}, params ...interface{}) (*types.Result, error)
+	Model(model ...interface{}) *Query
+	Select(model interface{}) error
+	Create(model ...interface{}) error
+	Update(model interface{}) error
+	Delete(model interface{}) error
+
+	Exec(query interface{}, params ...interface{}) (*types.Result, error)
+	ExecOne(query interface{}, params ...interface{}) (*types.Result, error)
 	Query(coll, query interface{}, params ...interface{}) (*types.Result, error)
 	QueryOne(model, query interface{}, params ...interface{}) (*types.Result, error)
+
 	QueryFormatter
 }
